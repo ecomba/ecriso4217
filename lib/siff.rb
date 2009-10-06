3be550cf61092b725eed383e02261618251a935e
Enrique Comba Riepenhausen <ecomba@nexwerk.com>
2009-10-06 22:36:41 +0100

    Currency class
    Introduced the currency object and added other helper methods to the module

--- lib/siff.rb
+++ lib/siff.rb
@@ -1,39 +1,66 @@
 module Siff
-  
-  SIFFS = {
-    :BE => :EUR,
-    :BG => :EUR,
-    :CA => :CAD,
-    :CZ => :EUR,
-    :DK => :EUR,
-    :DE => :EUR,
-    :EE => :EUR,
-    :IE => :EUR,
-    :EL => :EUR,
-    :ES => :EUR,
-    :FR => :EUR,
-    :GB => :GBP,
-    :IT => :EUR,
-    :CY => :EUR,
-    :LV => :EUR,
-    :LT => :EUR,
-    :LU => :EUR,
-    :HU => :EUR,
-    :MT => :EUR,
-    :NL => :EUR,
-    :AT => :EUR,
-    :PL => :EUR,
-    :PT => :EUR,
-    :RO => :EUR,
-    :SI => :EUR,
-    :SK => :EUR,
-    :FI => :EUR,
-    :SE => :EUR,
-    :UK => :GBP,
-    :US => :USD
-  }
-  
-  def currency_for(country_code, default_when_currency_not_found='unknown')
-    (SIFFS[country_code] || default_when_currency_not_found).to_s
+  class Currency
+    attr_reader :code, :sign
+
+    def initialize(code, sign)
+      @code, @sign = code, utf8(sign)
+    end
+
+    private
+    def utf8(string)
+      string.gsub(/U\+([0-9a-fA-F]{4,4})/u){["#$1".hex ].pack('U*')}
+    end
   end
+
+
+  EURO = Currency.new(:EUR, "U+20ac")
+  GBP = Currency.new(:GBP, "U+00a3")
+  USD = Currency.new(:USD, "U+0024")
+  GENERIC_SYMBOL = "U+00a4"
+
+  SIFFS = {
+    :BE => EURO, 
+    :BG => EURO,
+    :CA => Currency.new(:CAD, "CanU+0024"),
+    :CZ => EURO,
+    :DK => EURO,
+    :DE => EURO,
+    :EE => EURO,
+    :IE => EURO,
+    :EL => EURO,
+    :ES => EURO,
+    :FR => EURO,
+    :GB => GBP, 
+    :IT => EURO,
+    :CY => EURO,
+    :LV => EURO,
+    :LT => EURO,
+    :LU => EURO,
+    :HU => EURO,
+    :MT => EURO,
+    :NL => EURO,
+    :AT => EURO,
+    :PL => EURO,
+    :PT => EURO,
+    :RO => EURO,
+    :SI => EURO,
+    :SK => EURO,
+    :FI => EURO,
+    :SE => EURO,
+    :UK => GBP,
+    :US => USD
+  }
+
+  def currency_for(country_code, default_when_currency_not_found= 'unknown')
+    SIFFS[country_code] || Currency.new(default_when_currency_not_found,GENERIC_SYMBOL) 
+  end
+
+  def currency_code_for(country_code, default_when_currency_not_found= 'unknown')
+    currency_for(country_code, default_when_currency_not_found).code.to_s
+  end
+
+  def currency_sign_for(country_code, default_when_currency_not_found= 'unknown')
+    currency_for(country_code, default_when_currency_not_found).sign
+  end
+
 end
--- spec/siff_spec.rb
+++ spec/siff_spec.rb
@@ -1,41 +1,65 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Siff" do
+describe Siff do
   
   include Siff
   
-  context "finding currencies for countries" do
-    it "returns the currency of Germany" do
-      currency_for(:DE).should == 'EUR'
+  context "finding currency codes for known countries" do
+    it "returns the currency code of Germany" do
+      currency_code_for(:DE).should == 'EUR'
     end
     
-    it "returns the currency for the United Kingdom" do
-      currency_for(:UK).should == 'GBP'
-      currency_for(:GB).should == 'GBP'
+    it "returns the currency code for the United Kingdom" do
+      currency_code_for(:UK).should == 'GBP'
+      currency_code_for(:GB).should == 'GBP'
     end
     
-    it "returns the currency for Ireland" do
-      currency_for(:IE).should == 'EUR'
+    it "returns the currency code for Ireland" do
+      currency_code_for(:IE).should == 'EUR'
     end
     
-    it "returns the currency for Bulgaria" do
-      currency_for(:BG).should == 'EUR'
+    it "returns the currency code for Bulgaria" do
+      currency_code_for(:BG).should == 'EUR'
     end
     
-    it "returns the currency for United States of America" do
-      currency_for(:US).should == 'USD'
+    it "returns the currency code for United States of America" do
+      currency_code_for(:US).should == 'USD'
     end
     
-    it "returns the currency for Canada" do
-      currency_for(:CA).should == 'CAD'
+    it "returns the currency code for Canada" do
+      currency_code_for(:CA).should == 'CAD'
     end
+  end
+
+  context "finding currency codes or unknown countries" do
     
     it "returns 'unknown' if it doesn't know the currency" do
-      currency_for(:MARS).should == 'unknown'
+      currency_code_for(:MARS).should == 'unknown'
     end
-    
-    it "returns the requested default if it doesn't know the currency" do
-      currency_for(:MARS, 'GBP').should == 'GBP'
+   
+
+    it "returns the requested default currency code if it doesn't know the currency" do
+      currency_code_for(:MARS, 'GBP').should == 'GBP'
+    end
+  end
+
+  context "printing the sign of known currencies" do
+    it "returns the euro sign for the german (DE) currency" do
+      currency_sign_for(:DE).should == '€'
+    end
+
+    it "returns the dollar sign for the American dollar" do
+      currency_sign_for(:US).should == '$'
+    end
+
+    it "returns the canadian dollar sign for canadian dollars" do
+      currency_sign_for(:CA).should == "Can$"
+    end
+  end
+
+  context "printing the sign for unknown currency" do
+    it "returns the generic currency sign for unknown currencies" do
+      currency_sign_for(:MARS).should == "¤"
     end
   end
 end
